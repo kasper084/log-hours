35e4626614dbf82baaa2e9ac4114126657c9631a
kasper084 <kasper084@gmail.com>
2021-10-14 01:14:16 +0300

    added foreing keys

--- flyway-mod/src/main/resources/migration/V3__Creat_Project_Table.sql
+++ flyway-mod/src/main/resources/migration/V3__Creat_Project_Table.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS project;
 CREATE TABLE project
 (
     id                BIGSERIAL PRIMARY KEY,
-    organisation_name TEXT NOT NULL,
+    organisation_name TEXT NOT NULL REFERENCES organisation (name),
     description       TEXT NOT NULL,
     created_at        TIMESTAMP DEFAULT now(),
     updated_at        TIMESTAMP DEFAULT now()
--- flyway-mod/src/main/resources/migration/V4__Create_ProjectTask_Table.sql
+++ flyway-mod/src/main/resources/migration/V4__Create_ProjectTask_Table.sql
@@ -4,8 +4,8 @@ CREATE TABLE project_task
     id          BIGSERIAL PRIMARY KEY,
     project_id  BIGINT REFERENCES project (id),
     description TEXT NOT NULL,
-    employee_id BIGINT,
-    hours DOUBLE PRECISION,
+    employee_id BIGINT REFERENCES employee (id),
+    hours       DOUBLE PRECISION,
     created_at  TIMESTAMP DEFAULT now(),
     updated_at  TIMESTAMP DEFAULT now()
 );
